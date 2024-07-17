28234f26b76b449d3252cf5bcd15b8329413bebd
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-07-17 19:34:38 +0200

    Improve json format + add hcsr sensor

--- src/board/sensors.rs
+++ src/board/sensors.rs
@@ -1,21 +1,41 @@
 use esp_idf_hal::{adc::ADC1, gpio::*};
-use serde_json::json;
+use serde_json::{json, Map, Value};
 
-use crate::sensors::{battery_sensor::BatterySensor, moisture_sensor::MoistureSensor};
+use crate::sensors::{
+    battery_sensor::BatterySensor, hcsr04_sensor::HCSR04Sensor, moisture_sensor::MoistureSensor,
+};
 
 pub struct Sensors<'a> {
     #[cfg(feature = "moisture-sensor")]
     pub moisture_sensor: MoistureSensor<'a, ADC1, Gpio2, Gpio4>,
+
+    #[cfg(feature = "water-level-sensor")]
+    pub water_level_sensor: HCSR04Sensor<'a, Gpio10, Gpio0, Gpio1>,
 
     pub battery_sensor: BatterySensor<'a, ADC1, Gpio3>,
 }
 
 impl<'a> Sensors<'a> {
     pub fn to_json(&mut self) -> serde_json::Value {
+        let mut map = Map::new();
+
+        map.insert(
+            "battery".to_string(),
+            json!(self.battery_sensor.get_level()),
+        );
+
         #[cfg(feature = "moisture-sensor")]
-        json!( {
-                "battery": self.battery_sensor.get_level(),
-                "moisture": self.moisture_sensor.get_level(),
-        })
+        map.insert(
+            "moisture".to_string(),
+            json!(self.moisture_sensor.get_level()),
+        );
+
+        #[cfg(feature = "water-level-sensor")]
+        map.insert(
+            "water_level".to_string(),
+            json!({ "mm": self.water_level_sensor.get_distance_mm(),"level": self.water_level_sensor.get_level() }),
+        );
+
+        Value::Object(map)
     }
 }
