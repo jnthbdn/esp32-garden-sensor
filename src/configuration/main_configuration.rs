909721c072b59f458fa278635c6b6542f4931f3c
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-08-02 19:21:11 +0200

    Add maps addr server + water level + add url maker function

--- src/configuration/main_configuration.rs
+++ src/configuration/main_configuration.rs
@@ -1,10 +1,7 @@
 use super::nvs_configuration::*;
+use mutually_exclusive_features::exactly_one_of;
 
-// #[cfg(all(feature = "moisture-sensor", feature = "water-level-sensor"))]
-// compile_error!("Choose only one sensor.");
-
-#[cfg(not(any(feature = "moisture-sensor", feature = "water-level-sensor")))]
-compile_error!("Choose one sensor feature.");
+exactly_one_of!("moisture-sensor", "water-level-sensor");
 
 #[derive(Debug)]
 pub enum MapFormType {
@@ -37,6 +34,12 @@ pub const MAP_NVS_FORM: &[MapFormElement] = &[
         data_type: MapFormType::String("", 63),
     },
     MapFormElement {
+        nvs_key: &KEY_SERVER_ADDRESS,
+        form_name: "srvaddr",
+        template_id: Some("{ARVADDR}"),
+        data_type: MapFormType::String("", 128),
+    },
+    MapFormElement {
         nvs_key: &KEY_NAME,
         form_name: "name",
         template_id: Some("{NAME}"),
@@ -62,16 +65,40 @@ pub const MAP_NVS_FORM: &[MapFormElement] = &[
     },
     #[cfg(feature = "moisture-sensor")]
     MapFormElement {
-        nvs_key: &KEY_VHIGH,
+        nvs_key: &KEY_MOIST_VHIGH,
         form_name: "vhigh_moist",
         template_id: Some("{VHIGH_MOIST}"),
         data_type: MapFormType::Float(1.26),
     },
     #[cfg(feature = "moisture-sensor")]
     MapFormElement {
-        nvs_key: &KEY_VLOW,
+        nvs_key: &KEY_MOIST_VLOW,
         form_name: "vlow_moist",
         template_id: Some("{VLOW_MOIST}"),
         data_type: MapFormType::Float(2.55),
     },
+    #[cfg(feature = "water-level-sensor")]
+    MapFormElement {
+        nvs_key: &KEY_WATER_HIGH,
+        form_name: "water_high",
+        template_id: Some("{WATER_HIGH}"),
+        data_type: MapFormType::Float(20.0),
+    },
+    #[cfg(feature = "water-level-sensor")]
+    MapFormElement {
+        nvs_key: &KEY_WATER_LOW,
+        form_name: "water_low",
+        template_id: Some("{WATER_LOW}"),
+        data_type: MapFormType::Float(1020.0),
+    },
 ];
+
+pub fn make_http_url(config: &NvsConfiguration) -> String {
+    #[cfg(feature = "moisture-sensor")]
+    let endpoint = "send_soil_moisture";
+
+    #[cfg(feature = "water-level-sensor")]
+    let endpoint = "send_water_level";
+
+    format!("http://{}/{}", config.get_server_address(), endpoint)
+}
