dcb7f980a2c78a68e9f5171e2e7df017b8e84d41
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-10-26 18:40:45 +0200

    Remove board

--- src/board/buttons.rs
+++
@@ -1,5 +0,0 @@
-use esp_idf_svc::hal::gpio::*;
-
-pub struct Buttons<'a> {
-    pub settings: PinDriver<'a, Gpio5, Input>,
-}
--- src/board/on_board_led.rs
+++
@@ -1,6 +0,0 @@
-use esp_idf_svc::hal::gpio::*;
-
-pub struct OnBoardLed<'a> {
-    pub orange: PinDriver<'a, Gpio6, Output>,
-    pub green: PinDriver<'a, Gpio7, Output>,
-}
